80571f5a187e17621e9a5e2b2c0ed30c6ad312a7
Kelvin Chao <kelvinchao2011@gmail.com>
2022-02-17 23:03:16 -0800

    updated js with base cases

--- assets/app/app.js
+++ assets/app/app.js
@@ -17,19 +17,38 @@ const numer = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 const special = ['@', '%', '+', '\\', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.'];
 
 // Setting password in HTML
-function setPass(password) {
-  password_p.innerHTML = password;
+function setPass(text) {
+  password_p.innerHTML = text;
 }
 
 // creates random number selector for characters 
 // (sequence_len = total number of array items, len = length of password)
 function generator(seq_len) {
-  var keygener = [];
+  var password = [];
+  var sequence = lower;
 
   // sequencer generator
   // builds the sequence of letters/numbers/characters 
-  var sequence = lower;
   
+  // base case without any selection
+  if (numSelect.checked === false && 
+    uppSelect.checked === false && 
+    lowSelect.checked === false &&
+    specSelect.checked === false) {
+      
+      var sequence = [];
+      window.alert("Please select an option for the password generator")
+    }
+    
+    /* NEED TO ADD UPPER/LOWER SELECTION */
+  if (uppSelect.checked && lowSelect.checked === false) {
+      sequence = upper;
+  }
+
+  if (uppSelect.checked) {
+    sequence = sequence.concat(upper);
+}
+
   // if statements for selection
   if (numSelect.checked) {
     sequence = sequence.concat(numer);
@@ -39,21 +58,16 @@ function generator(seq_len) {
     sequence = sequence.concat(special);
   }
 
-  /* NEED TO ADD UPPER/LOWER SELECTION */
-  if (uppSelect.checked) {
-    sequence = sequence.concat(upper);
-  }
-
   // randomization loop
   // creates random numbers for sequence selection
   for (let i = 0; i<lenSlide.value; i++) {
-    keygener.push(
+    password.push(
       sequence[Math.floor(Math.random() * sequence.length)]
       )
   }
 
   // call setPass function
-  setPass(keygener.join(''))
+  setPass(password.join(''))
   /* console.log(sequence) */
 }
 
